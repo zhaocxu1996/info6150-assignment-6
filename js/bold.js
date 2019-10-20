7bbb50b71ed46da2beb8ac4476252e6488285ae4
zhaocxu1996 <zhaocxu1996@github.com>
2019-10-20 18:22:58 -0400

    add align function

--- js/bold.js
+++ js/bold.js
@@ -2,13 +2,11 @@ let boldBtn = document.getElementById("bold-btn");
 let boldClickListener = (event) => {
     event.preventDefault();
     let selection = window.getSelection().toString();
-
-    if (boldBtn.style.backgroundColor == "white") {
-        let final =`<span class ="text-bold">` + selection + `</span>`;
-        let completetext = document.getElementById("text-editor").innerHTML;
-        completetext= completetext.replace(selection,final);
-        document.getElementById("text-editor").innerHTML = completetext;
-    }
+    
+    let final =`<span class = "text-bold" id = "bold">` + selection + `</span>`;
+    let completetext = document.getElementById("text-editor").innerHTML;
+    completetext= completetext.replace(selection,final);
+    document.getElementById("text-editor").innerHTML = completetext;
 }
 
 boldBtn.addEventListener('click', boldClickListener);
